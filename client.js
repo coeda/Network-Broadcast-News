921d48f2956e66d0f96e930ab80944be37ecfad1
Casey Oeda <coeda@live.com>
2016-10-05 11:37:14 -1000

    completed flood and kick

--- client.js
+++ client.js
@@ -13,9 +13,20 @@ client.on('data', (data) => {
   console.log(data.toString());
 });
 
+function floodFunc(message){
+  client.write(message);
+}
+
 process.stdin.on('readable', () => {
   var chunk = process.stdin.read();
     if (chunk !== null) {
-      client.write(chunk);
+      chunk = chunk.toString();
+      if(chunk.slice(0, 6) === '/flood'){
+        setInterval( function() {floodFunc(chunk.slice(7)); }, 15);
+      } else {
+        client.write(chunk);
+      }
+
     }
 });
+
--- server.js
+++ server.js
@@ -1,27 +1,59 @@
 const net = require('net');
 let clients = [];
+let clientName = ['ADMIN'];
+let lastTime;
+let kicked = false;
 
 const server = net.createServer((request) => {
+  lastTime = Date.now();
   request.name = request.remoteAddress + ':' + request.remotePort;
   request.write('Welcome ' + request.name + '\n');
-  broadcast(request.name + ' has joined the chat', request.name);
   request.write('What is your name?');
 
   request.on('data', (data) => {
+    let currentTime = Date.now();
+    if((currentTime - lastTime) < 150){
+        let searchedClient = clientName.indexOf(request.name.toString());
+        if(searchedClient !== -1){
+          clients.forEach((client) => {
+            if(client.name.toString() === request.name.toString()){
+              kicked = true;
+              client.write('ADMIN: You have been kicked for flooding');
+              client.end();
+            }
+          });
+        }
+    } else {
       data.toString();
       data = data.slice(0, data.length - 1);
-    if(request.name === request.remoteAddress + ':' + request.remotePort){
-      request.name = data;
-      console.log('\n' + data);
-    } else {
-      broadcast(request.name + ': ' + data, request);
+      if(clientName.indexOf(data.toString()) !== -1){
+          return request.write('please enter a different name');
+      }
+      else if(request.name === request.remoteAddress + ':' + request.remotePort){
+          request.name = data;
+          clientName.push(data.toString());
+            broadcast(request.name + ' has joined the chat', request.name);
+      } else {
+        broadcast(request.name + ': ' + data, request);
+      }
+      lastTime = currentTime;
     }
 
+
   });
+
   request.on('end', () => {
     console.log('Connection Terminated');
     let selectedIndex = clients.indexOf(request);
+    let selectedName = clientName.indexOf(request.name.toString());
     clients.splice(selectedIndex, 1);
+    clientName.splice(selectedName, 1);
+    if(kicked === true){
+      broadcast(request.name + ' has been kicked');
+    } else {
+      broadcast(request.name + ' has left the chat');
+    }
+    kicked = false;
   });
 
 
@@ -33,7 +65,7 @@ const server = net.createServer((request) => {
         client.write(message);
       }
     });
-    process.stdout.write(message);
+    process.stdout.write(message + '\n');
   }
 
 
@@ -50,8 +82,22 @@ server.listen({ port: 6969, 'host': '0.0.0.0'}, () => {
 process.stdin.on('readable', () => {
   var chunk = process.stdin.read();
     if (chunk !== null) {
-      clients.forEach(function(client) {
-        client.write('ADMIN: ' + chunk);
-      });
+      chunk = chunk.toString();
+      if(chunk.slice(0,5) === '/kick'){
+        chunk = chunk.slice(6, chunk.length - 1);
+        let searchedClient = clientName.indexOf(chunk);
+        if(searchedClient !== -1){
+          clients.forEach((client) => {
+            if(client.name.toString() === chunk){
+              client.write('ADMIN: You have been kicked');
+              client.end();
+            }
+          });
+        }
+      } else {
+        clients.forEach(function(client) {
+          client.write('ADMIN: ' + chunk.slice(0, chunk.length -1));
+        });
+    }
     }
 });
